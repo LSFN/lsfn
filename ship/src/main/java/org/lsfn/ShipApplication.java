aca3794e9d81ab6b838be1e9b3f6434b90e51ac9
James Laverack <james@jameslaverack.com>
2017-07-22 14:24:08 +0100

    Split into ship and environment

--- ship/src/main/java/org/lsfn/ShipApplication.java
+++ ship/src/main/java/org/lsfn/ShipApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LsfnApplication {
+public class ShipApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LsfnApplication.class, args);
