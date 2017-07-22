19d0a518088e6d054aebf1bba8526797ceee727b
James Laverack <james@jameslaverack.com>
2017-07-22 16:06:14 +0100

    Add editorconfig, fix build

--- environment/src/main/java/org/lsfn/EnvironmentApplication.java
+++ environment/src/main/java/org/lsfn/EnvironmentApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EnvironmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnvironmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EnvironmentApplication.class, args);
+    }
 }
--- vessel/src/main/java/org/lsfn/VesselApplication.java
+++ vessel/src/main/java/org/lsfn/VesselApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VesselApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LsfnApplication.class, args);
+		SpringApplication.run(VesselApplication.class, args);
 	}
 }
