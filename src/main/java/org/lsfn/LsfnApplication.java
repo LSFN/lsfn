c403ed14fa23ed6a14620a48b6af9cd73127dcd9
James Laverack <james@jameslaverack.com>
2017-07-22 14:09:53 +0100

    Add Spring Initializr

---
+++ src/main/java/org/lsfn/LsfnApplication.java
@@ -0,0 +1,12 @@
+package org.lsfn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsfnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsfnApplication.class, args);
+	}
+}
