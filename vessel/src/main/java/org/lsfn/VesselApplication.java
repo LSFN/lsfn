026580ce338d8e7a56f2f5ff58f2659ae42d1249
James Laverack <james@jameslaverack.com>
2017-07-22 16:06:40 +0100

    Reformat project

--- vessel/src/main/java/org/lsfn/VesselApplication.java
+++ vessel/src/main/java/org/lsfn/VesselApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VesselApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VesselApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VesselApplication.class, args);
+    }
 }
--- vessel/src/test/java/org/lsfn/VesselApplicationTests.java
+++ vessel/src/test/java/org/lsfn/VesselApplicationTests.java
@@ -9,8 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class VesselApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
