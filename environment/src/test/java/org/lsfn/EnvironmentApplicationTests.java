18e1110e490a16dc9f2528a3a00c18526b1281b1
James Laverack <james@jameslaverack.com>
2017-07-22 16:16:48 +0100

    Rename test class

--- environment/src/test/java/org/lsfn/EnvironmentApplicationTests.java
+++ environment/src/test/java/org/lsfn/EnvironmentApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LsfnApplicationTests {
+public class EnvironmentApplicationTests {
 
     @Test
     public void contextLoads() {
