9ac88ba4607339d7b8dbfbdceb318b26cee0e697
James Laverack <james@jameslaverack.com>
2018-02-16 00:43:36 +0000

    Tokio Hello World

--- environment/src/main.rs
+++ environment/src/main.rs
@@ -1,3 +1,43 @@
+extern crate tokio;
+extern crate tokio_io;
+extern crate futures;
+
+use tokio::executor::current_thread;
+use tokio::net::TcpListener;
+use tokio_io::io;
+use futures::{Future, Stream};
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:6142".parse().unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
+
+    let server = listener.incoming().for_each(|socket| {
+        println!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
+
+
+        let connection = io::write_all(socket, "hello world\n")
+            .then(|res| {
+                println!("wrote message; success={:?}", res.is_ok());
+                Ok(())
+            });
+
+        // Spawn a new task that processes the socket:
+        current_thread::spawn(connection);
+
+        Ok(())
+    })
+        .map_err(|err| {
+            // All tasks must have an `Error` type of `()`. This forces error
+            // handling and helps avoid silencing failures.
+            //
+            // In our example, we are only going to log the error to STDOUT.
+            println!("accept error = {:?}", err);
+        });
+
+    current_thread::run(|_| {
+        // Now, the server task is spawned.
+        current_thread::spawn(server);
+
+        println!("server running on localhost:6142");
+    });
 }
