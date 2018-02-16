6deb4e679ca0c7b4fa4c2629b228dd2325d215c1
James Laverack <james@jameslaverack.com>
2018-02-16 00:06:21 +0000

    Throw out Java

--- environment/src/main/java/org/lsfn/comms/VesselComms.java
+++
@@ -1,9 +0,0 @@
-package org.lsfn.comms;
-
-import org.springframework.stereotype.Component;
-import org.lsfn.proto.envVessel.Downstream.Position;
-
-@Component
-public class VesselComms {
-    Position position;
-}
