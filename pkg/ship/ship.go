43e84bbc085ca133e56acbaf4e34f97e5c8c9d93
James Laverack <james@jameslaverack.com>
2019-01-24 01:40:55 +0000

    Correct comments

--- pkg/ship/ship.go
+++ pkg/ship/ship.go
@@ -1,22 +1,22 @@
 package ship
 
-// Information about the 3D rigid body that defines the ship in phyiscs
-// simulations
+// The Physics struct provides information about the 3D rigid body that defines
+// the ship in physics simulations
 type Physics struct {
 	Width  uint `yaml:"width"`
 	Height uint `yaml:"height"`
 	Length uint `yaml:"length"`
 }
 
-// A "part" is something that's on the ship. Generally this will expose
-// a number of controls and sensors, along with some functionality that
-// will impact the game.
+// A Part is something that's on the ship. Generally this will expose a number
+// of controls and sensors, along with some functionality that will impact the
+// game.
 type Part interface {
 	name() string
 }
 
-// This is the game engine's internal view of a ship. With all the information
-// needed to simulate it.
+// The Ship struct is the game engine's internal view of a ship. With all the
+// information needed to simulate it.
 type Ship struct {
 	Name    string  `yaml:"name"`
 	Physics Physics `yaml:"physics"`
